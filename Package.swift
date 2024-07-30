871b32866290e5d5e8d56d417eef168e49ab5ecb
HiteshMor <hiteshm@dewsolutions.in>
2024-07-31 01:22:20 +0530

    UIComponent bug fix
    (cherry picked from commit 3d6047d0c243e4408b2de483c5e2558ef2122a10)

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/Hitesh-mor/AppConstant.git", from: "0.0.1"),
+//         .package(url: "https://github.com/Hitesh-mor/AppConstant.git", from: "0.0.1"),
+         .package(path: "../AppConstant"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/UIComponentPackage/MakeOnlyUI.swift
+++ Sources/UIComponentPackage/MakeOnlyUI.swift
@@ -9,14 +9,15 @@ import SwiftUI
 import AppConstant
 
 public struct MakeOnlyUI: View {
-    let uiText: String?
-    
-    public init(uiText: String = "A Hello from UICoponent Package") {
-        self.uiText = uiText
-    }
+//    let uiText: String?
+//
+//    public init(uiText: String = "A Hello from UICoponent Package") {
+//        self.uiText = uiText
+//    }
+    public init() {}
     
     public var body: some View {
-        Text(AppConstant().text)
+        Text(AppConstant.text)
     }
 }
 
