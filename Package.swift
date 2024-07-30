97b209ea285d8265b95336f15d652cfea019ead2
Hitesh <morhitesh@gmail.com>
2024-07-31 01:33:53 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Hitesh-mor/AppConstant.git", from: "0.0.2"),
-         // .package(path: "../AppConstant"),
+         //  .package(path: "../AppConstant"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
